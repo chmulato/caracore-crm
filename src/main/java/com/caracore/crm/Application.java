56ab93b3c75731130c59f9b1be204bbe2c24a9fe
Christian Mulato <chrvlamulato.develcode@rd.com.br>
2023-11-10 09:57:05 -0300

    Config para war em prod

--- src/main/java/com/caracore/crm/Application.java
+++ src/main/java/com/caracore/crm/Application.java
@@ -1,8 +1,5 @@
 package com.caracore.crm;
 
-import com.vaadin.flow.component.dependency.NpmPackage;
-import com.vaadin.flow.server.PWA;
-import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -11,9 +8,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  * The entry point of the Spring Boot application.
  */
 @SpringBootApplication
-@NpmPackage(value = "lumo-css-framework", version = "^4.0.10")
-@Theme("caracore")
-@PWA(name = "Cara-Core CRM", shortName = "CRM", offlinePath="offline.html", offlineResources = { "./images/offline.png"})
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
